a208df61d926c3c18d6146788432336b40e99aad
jonaruiz <ruiz.jonathan2018@gmail.com>
2023-06-27 20:59:43 -0300

    Funcionalidad de mostrar turnos

--- VeterinariaOrt/Controllers/TurnosController.cs
+++ VeterinariaOrt/Controllers/TurnosController.cs
@@ -1,14 +1,18 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using VeterinariaOrt.Models;
 
 namespace VeterinariaOrt.Controllers
 {
     public class TurnosController : Controller
     {
+       DAO_Turnos DAO_turnos = new DAO_Turnos();
+        [HttpGet]
         [Authorize]
         public IActionResult Index()
         {
-            return View();
+            List<Turnos> turnos = DAO_turnos.ListarTurnos();
+            return View(turnos);
         }
     }
 }
---
+++ VeterinariaOrt/Models/DAO_Turnos.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Data.SqlClient;
+using VeterinariaOrt.Models;
+namespace VeterinariaOrt.Models
+
+{
+    public class DAO_Turnos
+    {
+        VeterinariaContext context = new VeterinariaContext();
+        public List<Turnos> ListarTurnos()
+        {
+            List<Turnos> turnos = new List<Turnos>();
+            try
+            {
+                string connectionString = "Data Source = DESKTOP-PLMOM31; initial catalog = Veterinaria ;Integrated Security = true ;Encrypt=true; TrustServerCertificate=true";
+                string sqlQuery = "SELECT * FROM Turnos";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Turnos turno = new Turnos();
+
+                                turno.Id = reader.GetInt32(0);
+                                turno.Dia = reader.GetString(1);
+                                turno.Hora = reader.GetString(2);
+
+                                turnos.Add(turno);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return turnos;
+        }
+    }
+}
